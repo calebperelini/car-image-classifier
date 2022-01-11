7dd5789692636654688b29e5a52c83a6a823690f
calebperelini <calebperelini@gmail.com>
2022-01-11 14:33:13 +1300

    Modified capture_frames for PIL Image Array

--- process_video.py
+++ process_video.py
@@ -1,17 +1,20 @@
 """
 Takes an mp4 file and outputs a series of images using OpenCV.
 """
+import requests
 import cv2
+from io import BytesIO
+from PIL import Image
 
-capture = cv2.VideoCapture("nz_cars_ALPR.mp4")
 
+def capture_frames() -> list:
 
-def capture_frames():
     capture = cv2.VideoCapture("nz_cars_ALPR.mp4")
 
     frame_counter = 0
     fps = 30
     sample_rate = 1  # Set sample rate to 1fps.
+    images = []
 
     while(capture.isOpened()):
 
@@ -19,11 +22,24 @@ def capture_frames():
         if not ret:
             break
         if frame_counter % (fps // sample_rate) == 0:
-            cv2.imwrite('data/test' + str(frame_counter) + '.jpg', frame)
+            images.append(Image.fromarray(frame))
         frame_counter += 1
 
     capture.release()
     cv2.destroyAllWindows()
+    return images
+
+
+def evaluate_images(image_array: list):
+    for img in image_array:
+        byte_io = BytesIO()
+        img.save(byte_io, 'png')
+        byte_io.seek(0)  # seek start of I/O stream.
+
+
+def main():
+    image_array = capture_frames()
+    evaluate_images(image_array)
 
 
 if __name__ == "__main__":
