cc2170dbd65a7935614741802f291359641a4b89
calebperelini <calebperelini@gmail.com>
2022-01-12 15:09:42 +1300

    testing SQLite.

--- database.py
+++ database.py
@@ -1,3 +1,28 @@
 import sqlite3
 
-connect = sqlite3.connect('vehicles.db')
+# connect to db
+conn = sqlite3.connect('vehicles.db')
+
+# create cursor
+c = conn.cursor()
+
+# create table for plate evaluations.
+
+# c.execute("""CREATE TABLE vehicles (
+#         plate_prediction text,
+#         score real,
+#         processing_time real
+#     )"""
+#           )
+
+c.execute("INSERT INTO vehicles VALUES ('KNH93', 0.92, 39.23)")
+
+conn.commit()
+
+c.execute("SELECT * FROM vehicles WHERE plate_prediction='KNH93'")
+
+print(c.fetchall())
+
+# commit command, close.
+conn.commit()
+conn.close()
