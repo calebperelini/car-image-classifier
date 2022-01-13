cdc2a09d4147dfc0bdb6d43c3ac4535f665462b2
calebperelini <calebperelini@gmail.com>
2022-01-13 16:54:01 +1300

    testing calls to the api.

--- carjam.py
+++ carjam.py
@@ -3,3 +3,17 @@ For handling requests to CarJam API and storing responses.
 """
 
 import requests
+import json
+
+response = requests.get('https://www.carjam.co.nz/api/availability/')
+
+args = {
+    'plate' : 'KNH93',
+    'basic' : 1
+}
+
+x = requests.post('https://test.carjam.co.nz/api/car/',
+    data= dict(basic=1)
+    )
+
+print(x.text)
