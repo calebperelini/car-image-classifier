c6cd0ab2bab2926b1044133f614be1db30ec66dd
calebperelini <calebperelini@gmail.com>
2022-01-17 10:29:28 +1300

    Carjam web scraper finished.

--- carjam_soup.py
+++ carjam_soup.py
@@ -1,18 +1,26 @@
-"""Web scraper alternative to API access.
+"""
+    Web scraper alternative to API access.
 """
 
 from bs4 import BeautifulSoup
 import requests
+import re
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-}
-r = requests.get('https://www.carjam.co.nz/car/?plate=KNH93', headers=headers).text
+def carjam_colour(plate: str) -> str:
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+    }
+    
+    r = requests.get('https://www.carjam.co.nz/car/?plate=' + plate.upper(), headers=headers).text
+    soup = BeautifulSoup(r, 'lxml')
+    car_colour_html = str(soup.findAll('span', {'class': 'value'})[3])
+    car_colour = re.findall( r'>(.*?)<' , car_colour_html)[0]
 
-soup = BeautifulSoup(r, 'lxml')
-car = soup.find(text = 'make')
-parent = car.parent
-make = parent.find()
-print(car)
+    return car_colour
+
+
+
+
+
 
 
