8a41eef7d511f6932f4ac0d6604d24cf536803ce
calebperelini <calebperelini@gmail.com>
2022-01-17 15:19:47 +1300

    Added model handler methods.

--- review_cars.py
+++ review_cars.py
@@ -1,1 +1,24 @@
+"""
+For passing images to the model for review, and comparing against carjam output.
+"""
 
+import os
+from model_eval import predict_image, model
+
+def get_filenames(path: str) -> list:
+    filenames_list = []
+    for filename in os.listdir(path):
+        file = os.path.join(path,filename)
+        if os.path.isfile(file):
+            filenames_list.append(file)
+    return filenames_list
+
+
+def get_predictions(filename_list: list) -> list:
+    predictions = []
+    for f in filename_list:
+        predictions.append(predict_image(model, f))
+    print(predictions)
+    print(len(predictions))
+
+get_predictions(get_filenames('images'))
