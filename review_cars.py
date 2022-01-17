42115a716b99b6d0309bc9ebdfa58cae26fec03e
calebperelini <calebperelini@gmail.com>
2022-01-17 16:56:52 +1300

    adding carjam methods.

--- review_cars.py
+++ review_cars.py
@@ -3,6 +3,8 @@ For passing images to the model for review, and comparing against carjam output.
 """
 
 import os
+import database
+import carjam_soup
 from model_eval import predict_image
 
 def get_filenames(path: str) -> list:
@@ -18,8 +20,25 @@ def get_predictions(filename_list: list) -> list:
     predictions = []
     for f in filename_list:
         predictions.append(predict_image(f))
-    for i in predictions:
-        print(i)
-    print(len(predictions))
+    return predictions
 
-get_predictions(get_filenames('images'))
+def compare_carjam(predictions: list):
+    db = database.retreive_all()
+    for pr in predictions:
+        for entry in db:
+            pr['plate_read'] = entry
+    
+    for car in predictions:
+        carj_colour = carjam_soup.carjam_colour(car['plate_read'][1])
+        if carj_colour is not None:
+            if carj_colour == car['colour']:
+                car['Match'] = True
+            else:
+                car['Match'] = False
+
+def main():
+    filenames = get_filenames('images')
+    compare_carjam(get_predictions(filenames))
+
+if __name__ == '__main__':
+    main()
